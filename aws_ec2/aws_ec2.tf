c4b74f444fc45c0e1229f866ee9b114a0b55a80c
RamCharanKolaDevelopment <ramcharankoladevelopment@gmail.com>
2026-02-19 21:21:58 +0530

    creating security group

--- aws_ec2/aws_ec2.tf
+++ aws_ec2/aws_ec2.tf
@@ -7,3 +7,29 @@ resource "aws_instance" "example" {
     Project = "roboshop"
   }
 }
+
+# Creating aws security group
+resource "aws_security_group" "allow_tls" {
+  name        = "allow-all-terraform" # this is for AWS account
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  egress { # outbound
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress { # inbound
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow-all-terraform"
+  }
+}
